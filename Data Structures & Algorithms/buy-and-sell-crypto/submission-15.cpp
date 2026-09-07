74725d4da782c4a3a12ff693380c1ea02c933bc4
dav1d <chiendinhbach9@gmail.com>
2026-09-07 18:44:23 -0500

    Add: buy-and-sell-crypto - submission-15

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-15.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProf = 0;
+
+        int i = 0;
+        int j;
+        if(prices.size() == 1)
+            return 0;    
+        while(i < prices.size()) {
+            j = i+1;
+            int prof;
+            while(j < prices.size() && prices[i] < prices[j] ) {
+                prof = prices[j] - prices[i];
+                maxProf = max(maxProf, prof);
+                j++;
+            }
+            i++;
+        }
+
+        return maxProf;
+    }
+};
